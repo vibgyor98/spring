962158cf3ffaa1f41f404330fe171a64f8dc06a8
Sourav Kar <www.sunny02@gmail.com>
2023-11-23 01:18:41 +0530

    introduced Beans to manage objects

--- src/main/java/com/vibgyor/learnspringframework/App01GamingBasicJava.java
+++ src/main/java/com/vibgyor/learnspringframework/App01GamingBasicJava.java
@@ -3,7 +3,7 @@ package com.vibgyor.learnspringframework;
 import com.vibgyor.learnspringframework.game.GameRunner;
 import com.vibgyor.learnspringframework.game.PacMan;
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
     public static void main(String[] args) {
 //        var game = new MarioGame();
         var game = new PacMan(); //Object creation
